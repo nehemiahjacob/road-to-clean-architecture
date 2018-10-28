98f9c8b2474bcdb274d4f8792e3c27cf434dc47f
Neville Tummon <neville.tummon@isentia.com>
2018-10-28 20:06:49 +1100

    create csv storage class in own module

--- app.py
+++ app.py
@@ -1,67 +1,17 @@
-import csv
-import os
-from datetime import date
-
-filename = "accounts.csv"
-file_exists = os.path.isfile(filename)
-field_names = ["account_nr", "date_opened", "balance"]
-
-def initialize():
-    with open(filename, "a") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=field_names)
-        if not file_exists:
-            writer.writeheader()
-
-def create_account(account_nr, balance):
-    with open(filename, "r") as csv_file:
-        reader = csv.DictReader(csv_file, fieldnames=field_names)
-
-        for row in reader:
-            if row["account_nr"] == account_nr:
-                raise ValueError("Account number already exists!")
-
-    with open(filename, "a") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=field_names)
-
-        writer.writerow({
-            "account_nr": account_nr,
-            "date_opened": date.today(),
-            "balance": balance
-        })
-
-def update_balance(account_nr, balance):
-    csv_rows = []
-    with open(filename, "r") as csv_file:
-        reader = csv.DictReader(csv_file, fieldnames=field_names)
-        for row in reader:
-            if row["account_nr"] == account_nr:
-                row["balance"] = balance
-            csv_rows.append(row)
-    
-    with open(filename, "w") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=field_names)
-        for row in csv_rows:
-            writer.writerow(row)
-
-def get_balance(account_nr):
-    with open(filename, "r") as csv_file:
-        reader = csv.DictReader(csv_file, fieldnames=field_names)
-
-        for row in reader:
-            if row["account_nr"] == account_nr:
-                balance = row["balance"]
-                return int(balance)
+from infrastructure.persistence import StorageCSV
 
 def main():
-    initialize()
+    filename = "accounts.csv"
+    storage_svc = StorageCSV(filename)
+    
     account_nr = input("Please give an account nr to create a new account: ")
 
     initial_balance = 0
-    create_account(account_nr, initial_balance)
+    storage_svc.create_account(account_nr, initial_balance)
 
     while True:
         try:
-            balance = get_balance(account_nr)
+            balance = storage_svc.get_balance(account_nr)
             print("")
             print("Your current balance for #{} is ${}".format(account_nr, balance))
             print("1) Deposit money into account")
@@ -97,7 +47,7 @@ def main():
                 else:
                     balance -= amount
             
-            update_balance(account_nr, balance)
+            storage_svc.update_balance(account_nr, balance)
 
         except KeyboardInterrupt:
             print("Exiting application!")
---
+++ infrastructure/persistence/local.py
@@ -0,0 +1,56 @@
+import csv
+import os
+from datetime import date
+
+class StorageCSV:
+    def __init__(self, filename):
+        self.filename = filename
+        self.field_names = ["account_nr", "date_opened", "balance"]
+        self._initialize()
+
+    def _initialize(self):
+        file_exists = os.path.isfile(self.filename)
+        with open(self.filename, "a") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=self.field_names)
+            if not file_exists:
+                writer.writeheader()
+
+    def create_account(self, account_nr, balance):
+        with open(self.filename, "r") as csv_file:
+            reader = csv.DictReader(csv_file, fieldnames=self.field_names)
+
+            for row in reader:
+                if row["account_nr"] == account_nr:
+                    raise ValueError("Account number already exists!")
+
+        with open(self.filename, "a") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=self.field_names)
+
+            writer.writerow({
+                "account_nr": account_nr,
+                "date_opened": date.today(),
+                "balance": balance
+            })
+
+    def update_balance(self, account_nr, balance):
+        csv_rows = []
+        with open(self.filename, "r") as csv_file:
+            reader = csv.DictReader(csv_file, fieldnames=self.field_names)
+            for row in reader:
+                if row["account_nr"] == account_nr:
+                    row["balance"] = balance
+                csv_rows.append(row)
+        
+        with open(self.filename, "w") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=self.field_names)
+            for row in csv_rows:
+                writer.writerow(row)
+
+    def get_balance(self, account_nr):
+        with open(self.filename, "r") as csv_file:
+            reader = csv.DictReader(csv_file, fieldnames=self.field_names)
+
+            for row in reader:
+                if row["account_nr"] == account_nr:
+                    balance = row["balance"]
+                    return int(balance)
